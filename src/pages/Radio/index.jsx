d11b1e13bc282791b3014846d7cd0535cc5738ba
phucbinh2001 <phucbinh.2001@gmail.com>
2022-01-01 16:04:21 +0700

    khai code đầu năm

--- src/pages/Radio/index.jsx
+++ src/pages/Radio/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./style.scss";
 
 function Radio() {
-  return <div>radio</div>;
+  return <div className="radio-page">Chúc mừng năm mới</div>;
 }
 
 export default Radio;
